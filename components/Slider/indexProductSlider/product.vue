5964d431b9f85191609258239ec746fc694b9ca3
nima <nimashahbazi524@gmail.com>
2021-04-30 16:20:01 +0430

    Some little changes

--- components/Slider/indexProductSlider/product.vue
+++ components/Slider/indexProductSlider/product.vue
@@ -3,7 +3,7 @@
     <div class="w-full h-full shadow-lg rounded-xl relative">
       <img src="/tp.jpg" alt="picture" class="object-center object-cover" />
       <div
-        class="absolute bottom-0 w-full py-3 z-20 w-1/2 bg-black opacity-50 text-white text-center"
+        class="absolute bottom-0 w-full py-3 z-20 bg-black opacity-50 text-white text-center"
       >
         <h1 class="text-2xl">Call Of Duty</h1>
       </div>
